fb26efbdb98a46934fe896fd12596f2e129ad604
phong198 <48425973+phong198@users.noreply.github.com>
2022-03-25 18:40:15 +0700

    250322

--- MoveStopMove/Assets/Script/Characters/EnemiesSpawner.cs
+++ MoveStopMove/Assets/Script/Characters/EnemiesSpawner.cs
@@ -8,7 +8,6 @@ public class EnemiesSpawner : MonoBehaviour
     private Vector3 position;
     private int xPos;
     private int zPos;
-    public bool isActive = false;
 
     private void Start()
     {
